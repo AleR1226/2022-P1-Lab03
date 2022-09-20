9206449267687d01b817f322d54eb6447a5fadcf
Ale <alejandrina.roman@vtsd.com>
2022-09-20 08:47:54 -0400

    Lab completed

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,23 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+	  int ms = 10000123;
+		System.out.println("starting Milliseconds:			"+ ms);
+
+		int h = ms / 3600000;
+		System.out.println("Hours:							" + h);
+
+		int hr=ms % 3600000;
+		int m = hr / 60000;
+		System.out.println("Minutes:						" + m);
+
+		int min = hr % 60000;
+		int s = min / 1000;
+		System.out.println("Seconds:						" + s);
+
+		int sr = min % 1000;
+		System.out.println("Milliseconds:					" + sr);
+
 	}
 }
 
